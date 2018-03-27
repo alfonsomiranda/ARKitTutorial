a4864c5489279dd051cda4a9890599a89f8fb584
Alfonso Miranda Castro <alfonso.miranda.castro@everis.com>
2018-03-27 16:08:00 +0200

    Initial commit

---
+++ ARKitTutorial/AppDelegate.swift
@@ -0,0 +1,29 @@
+//
+//  AppDelegate.swift
+//  ARKitTutorial
+//
+//  Created by Alfonso Miranda Castro on 29/1/18.
+//  Copyright © 2018 alfonsomiranda. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+    var rootViewController: UIViewController!
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+        
+        window = UIWindow(frame: UIScreen.main.bounds)
+        //rootViewController = ARObjectsPresentationAssembly.arObjectsViewController() //ARPresentationAssembly.arViewController()
+        rootViewController = ARViewController(nibName: "ARViewController", bundle: nil)
+        
+        window?.rootViewController = rootViewController
+        window?.makeKeyAndVisible()
+        
+        return true
+    }
+}
+
